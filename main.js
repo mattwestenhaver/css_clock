1adc2f4c6b2fc5603a184659a7a21a52486dae52
Matthew Westenhaver <matthew.westenhaver@gmail.com>
2018-07-02 00:16:40 -0700

    added seconds and if/else statements when min/secs is 0

--- main.js
+++ main.js
@@ -3,21 +3,34 @@ console.log('main.js connected')
 var d = new Date
 $('#date').text(d.toDateString())
 
-var msTimer = setInterval(myTimer, 30);
+var msTimer = setInterval(myTimer, 25);
 
 function myTimer() {
   var date = new Date
   var hours = date.getHours()
   var minutes = date.getMinutes()
+  var seconds = date.getSeconds()
   var millis = date.getMilliseconds()
 
   if (hours === 0) {
-    $("#hours").css("width", 35)
+    $("#hours").css("width", 26.25)
   } else {
-    $("#hours").css("width", `${hours * 35}`)
+    $("#hours").css("width", `${hours * 26.25}`)
   }
-  $("#minutes").css("width", `${minutes * 14}`)
-  $("#millis").css("width", `${millis * .84}`)
+
+  if (minutes === 0) {
+    $("#minutes").css("width", `${10.5}`)
+  } else {
+    $("#minutes").css("width", `${minutes * 10.5}`)
+  }
+
+  if (seconds === 0) {
+    $("#seconds").css("width", `${10.5}`)
+  } else {
+    $("#seconds").css("width", `${seconds * 10.5}`)
+  }
+
+  $("#millis").css("width", `${millis * .63}`)
 
   if (hours === 0) {
     $("#hours").text('12')
@@ -26,5 +39,7 @@ function myTimer() {
   } else if(hours > 0 && hours < 12) {
     $("#hours").text(`${hours}`)
   }
+  
   $("#minutes").text(`${minutes}`)
+  $("#seconds").text(`${seconds}`)
 }
